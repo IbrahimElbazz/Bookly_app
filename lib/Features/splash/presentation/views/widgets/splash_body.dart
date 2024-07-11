916139d4ee4de0514f50faaacfbd31034c0a543d
ibrahimElbaz <ibrahim.elbaz.1011@gmail.com>
2024-07-11 19:23:14 +0300

    add animation text in splash body

--- lib/Features/splash/presentation/views/widgets/splash_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_body.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:bookly_app/core/utils/images.dart';
 
-class splash_body extends StatelessWidget {
+class splash_body extends StatefulWidget {
   const splash_body({super.key});
+
+  @override
+  State<splash_body> createState() => _splash_bodyState();
+}
+
+class _splash_bodyState extends State<splash_body>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidanimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: (Duration(seconds: 2)),
+    );
+    slidanimation = Tween(begin: Offset(0, 20), end: Offset.zero)
+        .animate(animationController);
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +44,18 @@ class splash_body extends StatelessWidget {
           SizedBox(
             height: 5,
           ),
-          Text(
-            'Read free book',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 16),
+          AnimatedBuilder(
+            animation: slidanimation,
+            builder: (context, _) {
+              return SlideTransition(
+                position: slidanimation,
+                child: Text(
+                  'Read free book',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                ),
+              );
+            },
           ),
         ],
       ),
