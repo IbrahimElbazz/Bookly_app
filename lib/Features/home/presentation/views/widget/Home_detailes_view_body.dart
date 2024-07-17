214b8ff7c1955f4d4bd1c322c66bd21277e1ca4a
ibrahimElbaz <ibrahim.elbaz.1011@gmail.com>
2024-07-17 21:33:56 +0300

    custem scroll_view in details body and add SliverFillRemaining to controll screen have expanded

--- lib/Features/home/presentation/views/widget/Home_detailes_view_body.dart
+++ lib/Features/home/presentation/views/widget/Home_detailes_view_body.dart
@@ -15,59 +15,69 @@ class Book_detailes_view_body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const SafeArea(
-            child: const Book_detailes_custem_appBar(),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.28,
-            child: const custem_item(),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 3),
-            child: Text(
-              'The Book',
-              style: Style.title.copyWith(fontSize: 30),
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                const SafeArea(
+                  child: const Book_detailes_custem_appBar(),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.28,
+                  child: const custem_item(),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 3),
+                  child: Text(
+                    'The Book',
+                    style: Style.title.copyWith(fontSize: 30),
+                  ),
+                ),
+                Text(
+                  'The Book d',
+                  style: Style.title
+                      .copyWith(color: Colors.white.withOpacity(.75)),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Book_rating(),
+                const SizedBox(
+                  height: 32,
+                ),
+                const action_button(),
+                const Expanded(
+                  child: SizedBox(
+                    height: 50,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'You can also like',
+                      style: Style.title.copyWith(fontSize: 16),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                const list_view_item_inDetails(),
+                const SizedBox(
+                  height: 40,
+                )
+              ],
             ),
-          ),
-          Text(
-            'The Book d',
-            style: Style.title.copyWith(color: Colors.white.withOpacity(.75)),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Book_rating(),
-          const SizedBox(
-            height: 32,
-          ),
-          const action_button(),
-          const SizedBox(
-            height: 50,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'You can also like',
-                style: Style.title.copyWith(fontSize: 16),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          const list_view_item_inDetails(),
-          const SizedBox(
-            height: 40,
           )
         ],
       ),
